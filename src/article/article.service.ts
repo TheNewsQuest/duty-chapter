a2aa7937550344e8dd4b5bc0ae6ee683abda2a4e
bietdoikiem <minhlucky2408@gmail.com>
2022-05-30 18:52:21 +0700

    feat: add cursor pagination for article list

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,10 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Article, ArticleDocument } from './schemas/article.schema';
 
+export interface FindManyArticlesByCursorResponse {
+  cursor: {
+    datetime?: Date;
+    id?: string;
+  };
+  data: ArticleDocument[];
+}
+
+export interface ArticleCursor {
+  datetime: Date;
+  id: string;
+}
+
 @Injectable()
 export class ArticleService {
+  private readonly logger: Logger = new Logger(ArticleService.name);
+
   constructor(
     @InjectModel(Article.name)
     private readonly articleModel: Model<ArticleDocument>,
@@ -16,5 +35,80 @@ export class ArticleService {
    */
   async findAll(): Promise<Article[]> {
     return await this.articleModel.find().exec();
+  }
+
+  /**
+   * Find Many Articles from cursor
+   * @param datetime Datetime string (in ISO format)
+   * @param id Object's ID
+   * @returns Returned results from cursor (timestamp, id)
+   */
+  async findManyByCursor(
+    datetime: string,
+    id: string,
+    limit: number,
+  ): Promise<FindManyArticlesByCursorResponse> {
+    let result: ArticleDocument[] = [];
+    try {
+      if (!datetime || !id) {
+        result = await this.articleModel
+          .find({})
+          .sort({ postedAt: -1, _id: -1 })
+          .limit(limit)
+          .exec();
+        return {
+          cursor: this.getCursor(result, limit),
+          data: result,
+        };
+      }
+      const cursorDate = new Date(datetime);
+      result = await this.articleModel
+        .find({
+          $and: [
+            {
+              postedAt: { $lte: cursorDate },
+            },
+            {
+              $or: [
+                {
+                  postedAt: { $lt: cursorDate },
+                },
+                {
+                  _id: { $lt: id },
+                },
+              ],
+            },
+          ],
+        })
+        .sort({ postedAt: -1, _id: -1 })
+        .limit(limit)
+        .exec();
+      return {
+        cursor: this.getCursor(result, limit),
+        data: result,
+      };
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  /**
+   * Get cursor information from list of article results
+   * @param articles List of articles
+   * @param limit Limit
+   * @returns Cursor info
+   */
+  private getCursor(articles: ArticleDocument[], limit: number): ArticleCursor {
+    if (articles.length < limit) {
+      return {
+        datetime: null,
+        id: null,
+      };
+    }
+    return {
+      datetime: articles[limit - 1].postedAt,
+      id: articles[limit - 1]._id,
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable all CORS origins (at least for now)
+  app.enableCors();
+  // Specifying versioning (current v1)
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'api/v',
+  });
   await app.listen(process.env.APP_PORT || 4000);
   Logger.log(
     `Successfully connected to ${process.env.DATABASE_NAME} database`,
